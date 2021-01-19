843861f923c38d048679101180b8789260e60348
Thomas Mouchelet <mouchelet.thomas@gmail.com>
2021-01-19 18:20:54 +0100

    update firebase

--- databaseFirestore.js
+++ databaseFirestore.js
@@ -2,14 +2,15 @@ const firebase = require('firebase');
 require('dotenv').config();
 
 var firebaseConfig = {
-	apiKey: process.env.apiKey,
-	authDomain: process.env.authDomain,
-	databaseURL: process.env.databaseURL,
-	projectId: process.env.projectId,
-	storageBucket: process.env.storageBucket,
-	messagingSenderId: process.env.messagingSenderId,
-	appId: process.env.appId
+  apiKey: process.env.apiKey,
+  authDomain:process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 exports.db = firebase.firestore();
